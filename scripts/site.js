da410537e94f9c8e4acb3bc895f3a0cca84999a8
Andrew Lonsdale <andrew.lonsdale@lonsbio.com.au>
2016-01-25 09:31:24 +1100

    Add placeholder author information

--- scripts/site.js
+++ scripts/site.js
@@ -2226,9 +2226,8 @@ loadAndDisplayGist = function(gistId) {
       authorUrl = "http://journal-club-adventures.github.io";
       //date = "Jan 26, 2016";
       // author, title etc taken from the commit
-      description = "<a href=\"http://journal-club-adventures.github.io/\">Journal Club Adventures</a>. This is adventure <a href=\"http://journal-club-adventures.github.io/#" + gistId +"\">#" + gistId+"</a> by < href=\""+authorUrl+"\">"+author+"</a>.";
+      description = "<a href=\"http://journal-club-adventures.github.io/\">Journal Club Adventures</a>. This is adventure <a href=\"http://journal-club-adventures.github.io/#" + gistId +"\">#" + gistId+"</a> by <a href=\""+authorUrl+"\">"+author+"</a>.";
       
-
       footer = '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a> This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>. <br/> <br/>Built with <a href="http://cmx.io">Comix I/O</a>.';
 
 
